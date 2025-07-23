fffea72c42c2ed4316d284e983e197b1e3e89ab9
Soham Joshi <150990355+sojo06@users.noreply.github.com>
2025-07-23 11:07:30 +0530

    Update constants.ts

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,2 +1,2 @@
-// export const origin:String = "https://face-node.onrender.com"
-export const origin:String = "http://localhost:5000"
+export const origin:String = "https://face-node.onrender.com"
+// export const origin:String = "http://localhost:5000"
